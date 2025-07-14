702f80f1105b77aee63c4c8d4bc553a8ae02fd03
vikash <vikash.mrdevops@gmail.com>
2025-07-14 17:16:42 +0530

    shared libs

--- terraform/modules/bastion_host/bastion_user_data.sh
+++ terraform/modules/bastion_host/bastion_user_data.sh
@@ -10,3 +10,7 @@ sudo snap install helm --classic
 
 # Install Kubectl
 sudo snap install kubectl --classic
+
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -304,3 +304,22 @@ data "aws_instances" "eks_nodes" {
 
   depends_on = [aws_eks_node_group.mrdevops_demo_ng]
 }
+
+
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
+}
+
+data "tls_certificate" "oidc_thumbprint" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
